5baa9e210af3bed74645ef2d3a78badfe9b764f0
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2023-09-29 15:24:40 -0300

    Update ex4.php

--- TreinoPHP1/ex4.php
+++ TreinoPHP1/ex4.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Exercício 4</title>
-    <link rel="stylesheet" type="text/css" href="css/meuestilo.css">
+    <link rel="stylesheet" type="text/css" href="/meuestilo.css">
   </head>
   <body>
 
